5bed8374402d6242a181708f82ef026cab221690
Akash Dutta <dutta.akash1993official@gmail.com>
2020-04-26 00:45:51 +0530

    POST route added and api updated

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.port || 88;
 
 const tourJson = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+app.use(express.json());
+
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
@@ -15,6 +17,27 @@ app.get('/api/v1/tours', (req, res) => {
         }
     })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+
+    const newId = tourJson[tourJson.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tourJson.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tourJson), 
+    err => {
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        });
+    });
+    
+});
+  
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
